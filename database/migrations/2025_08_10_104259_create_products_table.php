b6db7aa59be33d252918ede39c36c4caaec3b7bf
Onur KINAY <ben@onurkinay.com.tr>
2025-08-10 17:35:18 +0300

    add thuminal

--- database/migrations/2025_08_10_104259_create_products_table.php
+++ database/migrations/2025_08_10_104259_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->json('images');
+            $table->string('image')->nullable();
             $table->string('name');
             $table->double('price');
             $table->string('short_description');
--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -40,6 +40,11 @@
                                                 placeholder="Product name">
                                         </div>
                                         <div class="flex flex-col">
+                                            <label class="leading-loose">Image</label>
+                                            <input type="file" name="image"
+                                                class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600">
+                                        </div>
+                                        <div class="flex flex-col">
                                             <label class="leading-loose">Images</label>
                                             <input type="file" name="images"
                                                 class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600">
