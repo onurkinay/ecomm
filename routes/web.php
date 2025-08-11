5dcfeb8943dcbc7e4ab1884eb100ba9d210404b1
Onur KINAY <ben@onurkinay.com.tr>
2025-08-11 12:15:17 +0300

    product home  blade

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductPageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductPageController::class, 'index'])->name('product.index');
 
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
